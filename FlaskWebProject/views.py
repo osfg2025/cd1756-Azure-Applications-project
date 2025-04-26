7f2687da5f40ad74b3459e406bfbd95b0f5630f7
osfg2025 <o.sinaolulana@chevron.com>
2025-04-26 18:50:39 +0100

    Update views.py
    to build req

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -134,7 +134,7 @@ def _build_msal_app(cache=None, authority=None):
         client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
+    ## TODO: Return the full Auth Request URL with appropriate Redirect URI
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state or str(uuid.uuid4()),
