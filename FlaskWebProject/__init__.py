dcd7a5f9599c12faaac53e58898a4ad49e49090b
osfg2025 <o.sinaolulana@chevron.com>
2025-04-28 00:27:30 +0100

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,7 +11,7 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+# Add any logging levels and handlers with app.logger
 # Setting logging level to info
 app.logger.setLevel(logging.INFO)
 # Creating stream handler for logging
@@ -27,4 +27,4 @@ login.login_view = 'login'
 
 import FlaskWebProject.views
 
-import FlaskWebProject.views
+##import FlaskWebProject.views
